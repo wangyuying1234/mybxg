3b0ed042d1782a95ae700fe15c9f2dad0bd3b3a3
wangyuying1234 <3462354318@qq.com>
2017-09-18 20:55:27 +0800

    完成讲师预览功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,4 +1,4 @@
-define(['jquery','template'],function($,template){
+define(['jquery','template','bootstrap'],function($,template){
 //请求后台接口获取列表数据
 $.ajax({
    type:'get',
@@ -8,6 +8,25 @@ $.ajax({
       //解析数据并渲染页面
      var html= template('teacherTpl',{list:data.result});
      $('#teacherInfo').html(html);
+     //绑定预览点击事件
+     $('.preview').click(function(){
+        //通过接口获取数据
+        var tcId=$(this).closest('td').attr('data-tcId');
+        $.ajax({
+            type:'get',
+            url:'/api/teacher/view',
+            data:{tc_id:tcId},
+            dataType:'json',
+            success:function(data){
+               //解析数据并渲染页面
+              var html=template('modalTpl',data.result);
+              console.log(html);
+              $('#modalInfo').html(html);
+              //显示弹窗
+              $('#teacherModal').modal();
+            }
+        });
+     });
    }
   });
 });
